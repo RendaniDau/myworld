2769177b813ff04ad237f59fd297e74add5c6866
rendani <rendanid@discovery.co.za>
2020-08-10 23:19:20 +0200

    Initial Commit - My Website

---
+++ src/test/java/za/co/rendani/myworld/MyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.rendani.myworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
